396ccbf2449168e36c366353ea574f9611a2fc7a
HarshJ23 <hjawajiwar@gmail.com>
2024-09-26 20:22:47 +0530

    frontend:err2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,27 @@
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i5.walmartimages.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-      },
-    ],
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'i5.walmartimages.com',
+//         port: '',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'firebasestorage.googleapis.com',
+//         port: '',
+//       },
+//     ],
+//   },
+// }
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
 }
+
+module.exports = nextConfig
